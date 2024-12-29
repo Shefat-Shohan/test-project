59ae366eac7f05187115ecddb3e549c757097593
Shefat-Shohan <shohan.ks3@gmail.com>
2024-12-29 20:53:25 +0600

    fix gallery

--- data/index.ts
+++ data/index.ts
@@ -1,4 +1,10 @@
 //grid images
+import SliderImage2 from "@/public/slider-2.jpg";
+import SliderImage3 from "@/public/slider-3.jpg";
+import SliderImage4 from "@/public/slider-4.jpg";
+import SliderImage5 from "@/public/slider-5.jpg";
+import SliderImage6 from "@/public/slider-6.jpg";
+import SliderImage7 from "@/public/slider-7.jpg";
 import GRIDIMAGE2 from "@/public/grid-image-2.jpg";
 import GRIDIMAGE3 from "@/public/grid-image-3.jpg";
 import GRIDIMAGE4 from "@/public/grid-image-4.jpg";
@@ -46,5 +52,48 @@ export const navBar = [
     id: 3,
     path: "/",
     menu: "Contact",
+  },
+];
+
+export const gallery = [
+  {
+    id: 1,
+    image: SliderImage2,
+  },
+  {
+    id: 2,
+    image: SliderImage3,
+  },
+  {
+    id: 3,
+    image: SliderImage4,
+  },
+  {
+    id: 4,
+    image: SliderImage5,
+  },
+  {
+    id: 5,
+    image: SliderImage6,
+  },
+  {
+    id: 6,
+    image: SliderImage7,
+  },
+  {
+    id: 7,
+    image: GRIDIMAGE2,
+  },
+  {
+    id: 8,
+    image: GRIDIMAGE3,
+  },
+  {
+    id: 9,
+    image: GRIDIMAGE4,
+  },
+  {
+    id: 10,
+    image: GRIDIMAGE5,
   },
 ];
